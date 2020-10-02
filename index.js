2674a67460d9b13a37c7eea7123d8d166f617642
Cheng Wang <cwaf17@gmail.com>
2020-10-01 22:04:21 -0400

    Get port from environment variable

--- index.js
+++ index.js
@@ -75,4 +75,7 @@ app.post("/send", (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("Server started. Listening at port 3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () =>
+  console.log(`Server started. Listening at port ${port}`)
+);
