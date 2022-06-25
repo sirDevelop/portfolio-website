be37e9b3b430397518d52bdfc21da1d3d40632a0
Chen W <50025994+sirDevelop@users.noreply.github.com>
2022-06-24 19:16:20 -0700

    add git lfs

--- public/generative_art_script.js
+++ public/generative_art_script.js
@@ -1,7 +1,8 @@
 $(function () {
     let videoPath = ["../public/animations/blood_pyramid_6_10_22.mp4", "../public/animations/striped_lines_6_10_22.mp4",
     "../public/animations/christmas_tree_6_10_22.mp4", "../public/animations/spider_web_6_11_22.mp4", "../public/animations/rainbow_6_11_22.mp4"
-    , "../public/animations/four_seasons_6_11_22.mp4", "../public/animations/quilt_6_23_22.mp4", "../public/animations/big_bang_6_24_22.mp4#t=1"];
+    , "../public/animations/four_seasons_6_11_22.mp4", "../public/animations/quilt_6_23_22.mp4", "../public/animations/big_bang_6_24_22.mp4#t=1",
+   "../public/animations/bouncing_ball_6_24_22.mp4#t=7"];
     
     // let thumbNailPath = ["../public/animations/thumbnails/Striped Lines.png", "../public/animations/thumbnails/blood pyramid.png",
     // "../public/animations/thumbnails/Christmas Tree.png", "../public/animations/thumbnails/Locking Web.png"];
