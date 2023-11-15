82c7e56f64a8ce80d3129dcb60c2f55727b21b6f
Chen W <50025994+sirDevelop@users.noreply.github.com>
2023-11-15 08:24:10 -0500

    Added new cert plus updated port code to avoid conflicts in port

--- index.js
+++ index.js
@@ -7,91 +7,106 @@ const cookieParser = require("cookie-parser");
 
 require("dotenv").config();
 
-const app = express();
-app.use(cookieParser());
+const net = require('net')
+const server = net.createServer()
+let port = 9000
 
-// View engine setup
-app.engine(
-  "handlebars",
-  exphbs({
-    extname: "hbs",
-    defaultLayout: "",
-    layoutsDir: "",
-  })
-);
-app.set("view engine", "handlebars");
+server.once('error', (e) => {
+  if (e.code === 'EADDRINUSE') {
+    port = port + 1
+    server.listen(port)
+  }
+})
+server.once('listening', function () { server.close() })
+server.listen(port)
+server.once('close', function () {
+  const app = express();
+  app.use(cookieParser());
 
-//Static folder
-app.use("/public", express.static(path.join(__dirname, "public")));
+  // View engine setup
+  app.engine(
+    "handlebars",
+    exphbs({
+      extname: "hbs",
+      defaultLayout: "",
+      layoutsDir: "",
+    })
+  );
+  app.set("view engine", "handlebars");
 
-// Body Parse Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+  //Static folder
+  app.use("/public", express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.render("personal");
-});
+  // Body Parse Middleware
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
-// app.get("/digitalart", (req, res) => {
-//   res.sendFile( __dirname + "/views/" + "digitalart.html" );
-// });
-
-app.get('s3url', (req, res) => {
-
-});
-
-app.get("/generativeart", (req, res) => {
-  res.sendFile( __dirname + "/views/" + "generativeart.html" );
-});
-/*
-app.post("/send", (req, res) => {
-  const output = `
-    <p>You have a new contact request</p>
-    <h3>Contact Details</h3>
-    <ul>
-        <li>Name: ${req.body.name} </li>
-        <li>Subject: ${req.body.subject} </li>
-        <li>Email: ${req.body.email} </li>
-    </ul>
-    <h3>Message</h3>
-    <p>${req.body.message}<p>
-    `;
-
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-    tls: {
-      rejectUnauthorized: false,
-    },
+  app.get("/", (req, res) => {
+    res.render("personal");
   });
 
+  // app.get("/digitalart", (req, res) => {
+  //   res.sendFile( __dirname + "/views/" + "digitalart.html" );
+  // });
 
-  let mailOptions = {
-    from: process.env.EMAIL,
-    to: process.env.EMAIL,
-    subject: "Node Contact Request",
-    text: "Message Submitted On Personal Website",
-    html: output,
-  };
+  app.get('s3url', (req, res) => {
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-    } else {
-      res.cookie("emailSuccess", true);
-    }
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    res.redirect("/");
   });
-});
-*/
-const port = process.env.PORT || 3000;
-app.listen(port, () =>
-  console.log(`Server started. Listening at port ${port}`)
-);
+
+  app.get("/generativeart", (req, res) => {
+    res.sendFile(__dirname + "/views/" + "generativeart.html");
+  });
+  /*
+  app.post("/send", (req, res) => {
+    const output = `
+      <p>You have a new contact request</p>
+      <h3>Contact Details</h3>
+      <ul>
+          <li>Name: ${req.body.name} </li>
+          <li>Subject: ${req.body.subject} </li>
+          <li>Email: ${req.body.email} </li>
+      </ul>
+      <h3>Message</h3>
+      <p>${req.body.message}<p>
+      `;
+  
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: false,
+      },
+    });
+  
+  
+    let mailOptions = {
+      from: process.env.EMAIL,
+      to: process.env.EMAIL,
+      subject: "Node Contact Request",
+      text: "Message Submitted On Personal Website",
+      html: output,
+    };
+  
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      } else {
+        res.cookie("emailSuccess", true);
+      }
+      console.log("Message sent: %s", info.messageId);
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      res.redirect("/");
+    });
+  });
+  */
+  port = process.env.PORT || port;
+  app.listen(port, () =>
+    console.log(`Server started. Listening at port ${port}`)
+  );
+
+})
